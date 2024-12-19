4c103f63434ffd07380aa7d81473346856976899
itou aiki <itouaiki46@gmail.com>
2024-12-19 12:10:03 +0900

    BlockBaseの作成
    ブロックの基底クラスの作成

---
+++ Mario1-1/Source/BaseClass/BlockBase.cpp
@@ -0,0 +1,21 @@
+#include "BlockBase.h"
+
+BlockBase::BlockBase(Vector2 position_, int handle_):ObjectBase(handle_,position_, TAG)
+{
+
+}
+
+BlockBase::~BlockBase()
+{
+
+}
+
+void BlockBase::Update()
+{
+	Move();
+}
+
+void BlockBase::Move()
+{
+
+}
---
+++ Mario1-1/Source/BaseClass/BlockBase.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "ObjectBase.h"
+
+class BlockBase : public ObjectBase
+{
+public:
+	// メンバ関数
+	BlockBase(Vector2 position_, int handle_); // コンストラクタ
+	~BlockBase(); // デストラクタ
+	void Update() override; // 更新処理
+
+private:
+	// メンバ関数
+	void Move(); // スクロール処理
+
+	// メンバ定数
+	const std::string TAG = "Block"; // ObjectBaseのtag(第三引数)に渡す
+
+};
