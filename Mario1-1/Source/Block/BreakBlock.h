f00e0da68800a81db27b26cec54a990e09d3b0b6
itou aiki <itouaiki46@gmail.com>
2024-12-20 16:42:51 +0900

    BreakBlock.hの追加
    必要な関数と変数を追加

--- Mario1-1/Source/Block/BreakBlock.h
+++ Mario1-1/Source/Block/BreakBlock.h
@@ -1,1 +1,18 @@
 #pragma once
+#include "..\BaseClass\\BlockBase.h"
+
+class BreakBlock : public BlockBase
+{
+public:
+	// メンバ関数
+	BreakBlock(); // コンストラクタ
+	~BreakBlock(); // デストラクタ
+	void Update() override; // 更新処理
+	void Draw() override; // 描画処理
+	void OnCollision(const CollideResult& result_) override; // 衝突時の処理
+
+private:
+
+	// メンバ変数
+	int imageHandle; // 画像ハンドル
+};
