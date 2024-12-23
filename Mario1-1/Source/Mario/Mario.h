ff838076adff2425f7eccda4617bb81d22ece00d
awatayuuto <yulovecat9@gmail.com>
2024-12-23 15:55:34 +0900

    マリオクラスの作成・DebugObjectの修正
    ・Marioフィルターを追加
    ・Marioクラスのヘッダーとcppファイルを作成
    ・DebugObjectのUpdateの二重定義エラーを解消

---
+++ Mario1-1/Source/Mario/Mario.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "..\BaseClass\ObjectBase.h"
+
+class Mario : public ObjectBase {
+	//メンバ関数
+public:
+	//コンストラクタ
+	Mario(Vector2 position_);
+	//デストラクタ
+	~Mario();
+
+
+private:
+
+};
