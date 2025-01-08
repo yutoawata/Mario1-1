c69fb175ce6735038240fa6805e948e32b805854
itou aiki <itouaiki46@gmail.com>
2025-01-08 15:31:33 +0900

    デバッグ用の入力移動処理の実装

--- Mario1-1/Source/BaseClass/BlockBase.cpp
+++ Mario1-1/Source/BaseClass/BlockBase.cpp
@@ -18,4 +18,26 @@ void BlockBase::Update()
 void BlockBase::Move()
 {
 	// スクロール処理
+
+	// デバッグ用入力
+	if (CheckHitKeyAll != 0)
+	{
+		if (CheckHitKey(KEY_INPUT_W))
+		{
+			position.y--;
+		}
+		else if (CheckHitKey(KEY_INPUT_S))
+		{
+			position.y++;
+		}
+
+		if (CheckHitKey(KEY_INPUT_A))
+		{
+			position.x--;
+		}
+		else if (CheckHitKey(KEY_INPUT_D))
+		{
+			position.x++;
+		}
+	}
 }
