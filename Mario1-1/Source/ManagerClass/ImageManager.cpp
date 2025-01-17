b41f0e4b2a3684cf4412d1ce06219c0381625a16
itou aiki <itouaiki46@gmail.com>
2025-01-17 14:55:03 +0900

    ミニマリオの画像処理の追加

--- Mario1-1/Source/ManagerClass/ImageManager.cpp
+++ Mario1-1/Source/ManagerClass/ImageManager.cpp
@@ -1,21 +1,26 @@
 #include "ImageManager.h"
 
+// ブロック系
 int ImageManager::fGroundBlockHnadle = 0;
 int ImageManager::fBrickBlockHandle[2] = { 0,0 };
 int ImageManager::bGroundBlockHandle = 0;
 int ImageManager::bBrickBlockHandle = 0;
 int ImageManager::unBreakBlockHandle = 0;
 int ImageManager::questionBlockHandle[4] = { 0,0,0,0 };
+// アイテム系
 int ImageManager::fCoinHandle[3] = { 0,0,0 };
 int ImageManager::bCoinHandle[3] = { 0,0,0 };
 int ImageManager::mushroomHandle = 0;
 int ImageManager::oneUpMushroomHandle = 0;
 int ImageManager::starHandle[4] = { 0,0,0,0 };
+// キャラクター系
+int ImageManager::normalMarioHandle[9] = { 0,0,0,0,0,0,0,0,0 };
 int ImageManager::goombaHandle[3] = { 0,0,0 };
 int ImageManager::koopaTroopaHandle[4] = { 0,0,0,0 };
 
 void ImageManager::LoadImages()
 {
+	// ブロック系
 	fGroundBlockHnadle = LoadGraph("././Resource/Images/fGroundBlock.png");
 	if (fGroundBlockHnadle == -1)
 	{
@@ -52,6 +57,8 @@ void ImageManager::LoadImages()
 		printfDx("ERROR:questionBlockHandle");
 		return;
 	}
+
+	// アイテム系
 	LoadDivGraph("././Resource/Images/Coins_01.png", 3, 3, 1, 32, 32, fCoinHandle);
 	if (fCoinHandle[0] == -1)
 	{
@@ -82,6 +89,14 @@ void ImageManager::LoadImages()
 		printfDx("ERROR:starHandle");
 		return;
 	}
+
+	// キャラクター計
+	LoadDivGraph("././Resource/Images/NormalMario.png", 9, 3, 3, 32, 32, normalMarioHandle);
+	if (normalMarioHandle[0] == -1)
+	{
+		printfDx("ERROE:normalMarioHandle");
+		return;
+	}
 	LoadDivGraph("././Resource/Images/Goomba.png", 3, 3, 1, 32, 32, goombaHandle);
 	if (goombaHandle[0] == -1)
 	{
@@ -100,17 +115,21 @@ void ImageManager::LoadImages()
 
 void ImageManager::DeleteImages()
 {
+	// ブロック系
 	DeleteGraph(fGroundBlockHnadle);
 	DeleteImageArray(fBrickBlockHandle, 2);
 	DeleteGraph(bGroundBlockHandle);
 	DeleteGraph(bBrickBlockHandle);
 	DeleteImageArray(questionBlockHandle, 4);
 	DeleteGraph(unBreakBlockHandle);
+	// アイテム系
 	DeleteImageArray(fCoinHandle, 3);
 	DeleteImageArray(bCoinHandle, 3);
 	DeleteGraph(mushroomHandle);
 	DeleteGraph(oneUpMushroomHandle);
 	DeleteImageArray(starHandle, 4);
+	// キャラクター系
+	DeleteImageArray(normalMarioHandle, 9);
 	DeleteImageArray(goombaHandle, 3);
 	DeleteImageArray(koopaTroopaHandle, 4);
 }
