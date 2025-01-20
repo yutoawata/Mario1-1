bffef646cc339919da63df28291a3f002de41edf
itou aiki <itouaiki46@gmail.com>
2025-01-20 12:59:25 +0900

    マップオブジェクトの追加
    ゴールポールと旗

---
+++ Mario1-1/Source/MapObject/Pole.cpp
@@ -0,0 +1,22 @@
+#include "Pole.h"
+
+Pole::Pole(Vector2 position_, int image_handle):ObjectBase(position_,image_handle,"Pole"),imageHandle(image_handle)
+{
+
+}
+
+Pole::~Pole()
+{
+
+}
+
+void Pole::Update()
+{
+	debugBlockMove->Update();
+	position += debugBlockMove->GetMove_Val();
+}
+
+void Pole::Draw()
+{
+	DrawGraph(position.x, position.y, imageHandle, TRUE);
+}
